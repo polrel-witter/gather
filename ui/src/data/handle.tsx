46b1ec6b0887b50264cf387688d202461927ec0d
username <bendez95@protonmail.com>
2022-08-02 11:33:35 +0200

    modified folder structure

--- ui/src/data/handle.tsx
+++
@@ -1,5 +0,0 @@
-export const handleTodo = (newState : any) => { 
-		console.log('state:');
-		console.log(newState);
-		return [];
-}
--- ui/src/data/subscribe.tsx
+++
@@ -1,5 +0,0 @@
-import Urbit from '@urbit/http-api';
-const api = new Urbit('', '', window.desk);
-api.ship = window.ship;
-
-export const subTodo = async (handle : any) => await api.subscribe({app: 'org', path:'/updates', err: handle, event:handle, quit:handle});
